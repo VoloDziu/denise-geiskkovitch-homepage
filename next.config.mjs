6e11761b8db13224ee132574b6166d857faba579
Volodymyr Dziubak <Volodymyr.Dzyubak@gmail.com>
2025-02-16 20:48:28 -0500

    favicon + wrangler

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 import rehypePrism from '@mapbox/rehype-prism'
 import nextMDX from '@next/mdx'
 import remarkGfm from 'remark-gfm'
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -19,5 +20,9 @@ const withMDX = nextMDX({
     rehypePlugins: [rehypePrism],
   },
 })
+
+if (process.env.NODE_ENV === 'development') {
+  await setupDevPlatform()
+}
 
 export default withMDX(nextConfig)
