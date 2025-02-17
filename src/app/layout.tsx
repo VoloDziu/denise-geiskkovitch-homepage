f421ff5176c291ae8b02dac0e9080bbcaaae8cdf
Volodymyr Dziubak <Volodymyr.Dzyubak@gmail.com>
2025-02-16 20:56:58 -0500

    try another approach for ico

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="h-full antialiased" suppressHydrationWarning>
+      <head>
+        <link rel="icon" href="/icon.ico" sizes="any" />
+      </head>
       <body className="flex h-full bg-zinc-50 dark:bg-black">
         <Providers>
           <div className="flex w-full">
