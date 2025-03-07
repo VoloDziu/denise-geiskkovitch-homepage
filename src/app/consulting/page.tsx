6e9fae6304ae9d65fce238036a6f76182fe1a6db
Denise Geiskkovitch <denise.geiskkovitch@gmail.com>
2025-03-06 20:28:30 -0500

    updated consulting page

--- src/app/consulting/page.tsx
+++ src/app/consulting/page.tsx
@@ -48,9 +48,9 @@ function MailIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
 }
 
 export const metadata: Metadata = {
-  title: 'About',
+  title: 'Consulting',
   description:
-    'I’m Spencer Sharp. I live in New York City, where I design the future.',
+    'I help companies improve their products, processes, and outcomes through technology consulting.',
 }
 
 export default function Consulting() {
@@ -69,35 +69,23 @@ export default function Consulting() {
         </div>
         <div className="lg:order-first lg:row-span-2">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">
-            I’m Spencer Sharp. I live in New York City, where I design the
-            future.
+            Get the expert results you want.
           </h1>
           <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
             <p>
-              I’ve loved making things for as long as I can remember, and wrote
-              my first program when I was 6 years old, just two weeks after my
-              mom brought home the brand new Macintosh LC 550 that I taught
-              myself to type on.
+              I help small, medium, and large size enterprises improve their
+              products, processes, and outcomes through:
             </p>
+            <ul>
+              <li>User Experience, Usability Testing, and User Research</li>
+              <li>Internal Workflow and Interface Optimization</li>
+              <li>Interaction Design Analysis and Feedback</li>
+              <li>Technology Adoption Planning and Strategy</li>
+            </ul>
+
             <p>
-              The only thing I loved more than computers as a kid was space.
-              When I was 8, I climbed the 40-foot oak tree at the back of our
-              yard while wearing my older sister’s motorcycle helmet, counted
-              down from three, and jumped — hoping the tree was tall enough that
-              with just a bit of momentum I’d be able to get to orbit.
-            </p>
-            <p>
-              I spent the next few summers indoors working on a rocket design,
-              while I recovered from the multiple surgeries it took to fix my
-              badly broken legs. It took nine iterations, but when I was 15 I
-              sent my dad’s Blackberry into orbit and was able to transmit a
-              photo back down to our family computer from space.
-            </p>
-            <p>
-              Today, I’m the founder of Planetaria, where we’re working on
-              civilian space suits and manned shuttle kits you can assemble at
-              home so that the next generation of kids really <em>can</em> make
-              it to orbit — from the comfort of their own backyards.
+              If you'd like to work with me, you can reach out at{' '}
+              <a href="mailto:geiskkod@mcmaster.ca">geiskkod@mcmaster.ca</a>
             </p>
           </div>
         </div>
